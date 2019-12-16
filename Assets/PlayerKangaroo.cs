de70290d802e42e1c30f03aacd73bac4d9fd2cf1
Sirmaka <ldjr22@gmail.com>
2019-12-16 22:15:44 +1100

    start changing script, altered animator

--- Assets/PlayerKangaroo.cs
+++ Assets/PlayerKangaroo.cs
@@ -11,7 +11,9 @@ public class PlayerKangaroo : MonoBehaviour
     public float jumpPower;
     public float xdrag;
     public bool isPunching;
-
+    public bool canMove; //if false, stops player movement inputs from working.
+    public int punchCharge = 200;
+    public int punchState = 0; //set to 0 for first state, 1 for punching state and 2 for end state. -1 for if not punching.
     public Animator thisAnimator;
 
     private Rigidbody rb;
@@ -22,27 +24,32 @@ public class PlayerKangaroo : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         disGround = GetComponent<Collider>().bounds.extents.y;
         isPunching = false;
+        canMove = true;
+        punchState = -1;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if(Input.GetButton("Fire1")
-        && (!isPunching) )
-        {
-            isPunching = true;
-            thisAnimator.SetBool("isPunching", true);
-        }
-        float moveHor = 0;
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            moveHor = -1;
-            this.GetComponent<SpriteRenderer>().flipX = false;
-        }
-        else if (Input.GetKey(KeyCode.RightArrow))
-        {
-            moveHor = 1;
-            this.GetComponent<SpriteRenderer>().flipX = true;
+        float moveHor = 0;//moved this here
+        if(canMove){//just to check if player inputs are allowed.
+            if(Input.GetButton("Fire1")
+            && (!isPunching) )
+            {
+                isPunching = true;
+                StartPunching();
+            }
+            //float moveHor = 0; //moved outside of scope
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                moveHor = -1;
+                this.GetComponent<SpriteRenderer>().flipX = false;
+            }
+            else if (Input.GetKey(KeyCode.RightArrow))
+            {
+                moveHor = 1;
+                this.GetComponent<SpriteRenderer>().flipX = true;
+            }
         }
         /*
         if(moveHorCur != moveHor)
@@ -109,6 +116,17 @@ public class PlayerKangaroo : MonoBehaviour
         }
         */
     }
+
+    public void StartPunching(){
+        if(punchCharge <= 0){
+            punchPhase2();//redirects to next phase
+        }
+        thisAnimator.SetInteger("punchState", 0);
+        canMove = false;
+        punchCharge -= 1;
+        
+    }
+
     public void EndPunchAnimation()
     {
         thisAnimator.SetBool("isPunching", false);
