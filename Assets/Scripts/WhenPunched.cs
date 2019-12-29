0f887d3f0ab408b69f39485473fc9b8c15ce1978
Sirmaka <ldjr22@gmail.com>
2019-12-29 18:18:09 +1100

    made some additions

---
+++ Assets/Scripts/WhenPunched.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhenPunched : MonoBehaviour
+{
+    private Rigidbody rb;
+    private Transform trans;
+    private GameObject player;
+    public Animator thisAnimator;
+    public bool playerPunching = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        trans = GetComponent<Transform>();
+
+        if (GameObject.Find("Kangaroo"))
+        {//if the kangaroo is in the scene
+            player = GameObject.Find("Kangaroo");
+            playerPunching = player.GetComponent<PlayerKangaroo>().isPunching;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
